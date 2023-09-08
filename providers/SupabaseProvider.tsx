a0bad839c9db002db8ddde3dcf9e158d80b094c9
James Aguirre <jamesaguirre98@gmail.com>
2023-09-08 01:56:43 -0700

    added db schema, provider

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -3,13 +3,21 @@
 import { Database } from "@/types_db";
 import { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 interface SupabaseProverProps {
   children: React.ReactNode;
 }
 
 const SupaBaseProvider: React.FC<SupabaseProverProps> = ({ children }) => {
-  const [supabaseClient] = useState(
-    () => createClientComponentClient<Database>
+  const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
   );
 };
+
+export default SupaBaseProvider;
